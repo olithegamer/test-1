953487f526e0f6c30f657c30b223a71e9a96a575
olithegamer <oliverjudeslone@gmail.com>
2025-08-31 12:47:30 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@
 #include <iostream>
 
 int main() {
-std::cout << "hello world";
+std::cout << "Choose a number";
+    int number_1;
+    std::cin >> number_1;
+    std::cout << "Choose another number";
+    int number_2;
+    std::cin >> number_2;
+    int number_sum;
+    number_sum = number_1 + number_2;
+    std::string number_sum_string = std::to_string(number_sum);
+    std::cout << "The sum of those numbers is "+number_sum_string;
 return 0;
 }
